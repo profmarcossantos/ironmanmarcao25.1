dfe8cfd89aaaf732bcefdd0bf2475882fd171ded
Prof. Marcos <marcos.santos@atitus.edu.br>
2025-05-12 20:12:59 -0300

    Chefe pediu para por nome do criador do jogo

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tkinter import messagebox
 from recursos.funcoes import inicializarBancoDeDados
 from recursos.funcoes import escreverDados
 import json
-print("Inicializando o Jogo!")
+print("Inicializando o Jogo! Criado por Marcão")
 pygame.init()
 inicializarBancoDeDados()
 tamanho = (800,600)
